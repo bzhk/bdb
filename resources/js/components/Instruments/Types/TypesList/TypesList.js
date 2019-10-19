a21444f59186ae74eec69590bf43c7bdc747bd88
Bartosz <bzhk911@gmail.com>
2019-10-19 22:01:49 +0200

    remove for typesList

--- resources/js/components/Instruments/Types/TypesList/TypesList.js
+++ resources/js/components/Instruments/Types/TypesList/TypesList.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import TypeItem from "./TypeItem/TypeItem";
-
+import RemoveModal from "../../../Helpers/RemoveModal";
 const TypesList = ({
     typesList,
     removeType,
     setNewInstrumentModal,
     setTypeId
 }) => {
+    const [removeModal, setRemoveModal] = useState(false);
+    const [removeId, setRemoveId] = useState(null);
     return (
         <div>
+            {removeModal && (
+                <RemoveModal
+                    label={"Czy chcesz usunąć typ instrumentu?"}
+                    actionCancel={() => {
+                        setRemoveModal(false);
+                        setRemoveId(null);
+                    }}
+                    actionOK={() => {
+                        removeType(removeId);
+                        setRemoveModal(false);
+                    }}
+                />
+            )}
             {typesList.map(elem => {
                 return (
                     <TypeItem
                         key={elem.id}
                         elem={elem}
-                        removeType={() => removeType(elem.id)}
+                        removeType={() => {
+                            setRemoveId(elem.id);
+                            setRemoveModal(true);
+                        }}
                         setNewInstrumentModal={() => {
                             setTypeId(elem.id);
                             setNewInstrumentModal(true);
